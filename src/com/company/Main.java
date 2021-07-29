2682cfdb98e4ebf729e1e386455e4dbe91b8106d
Artemleonenkoav <leonenkoavfin@gmail.com>
2021-07-29 11:51:14 +0300

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,12 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        String name = "myname";
+        int age = 25;
+        double desiredSalary = 5.1;
+        char gender = 'f';
+        boolean lookingForJob = true;
+    }
+}
